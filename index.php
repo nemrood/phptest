c74411db3d1a1a959fd97a2c386f7078163e4b16
nemrood <41047129+nemrood@users.noreply.github.com>
2019-06-18 08:30:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-echo 'Bonjour'; ?>
+
+mysql_connect('relais-operations-cit.siege.intra.groupe-casino.fr:3306', 'root', 'sogeti') or die('Could not connect the database : Username or password incorrect');
+mysql_select_db('relais') or die ('No database found');
+echo 'Database Connected successfully';
+?>
